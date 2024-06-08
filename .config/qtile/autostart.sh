608b8e4ef61968be4bbdf224004ef4a5b5f9e6ea
Fahim Fuad Prottoy <46813360+prottoyfuad@users.noreply.github.com>
2024-06-08 07:51:08 +0600

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,21 +1,15 @@
+#!/bin/sh
 
-#!/usr/bin/env bash 
+xrandr -s 1920x1080
+
+nm-applet &
+
+picom &
+nitrogen --restore &
+#volumeicon &
 
 # festival --tts $HOME/.config/qtile/welcome_msg &
 # lxsession &
-picom &
 # /usr/bin/emacs --daemon &
 # conky -c $HOME/.config/conky/qtile/doom-one-01.conkyrc
-# volumeicon &
-# nm-applet &
-
-### UNCOMMENT one of these three for wallpaper
-# 1. Uncomment to restore last saved wallpaper
-# xargs xwallpaper --stretch < ~/.cache/wall &
-# 2. Uncomment to set a random wallpaper on login
-# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
-# 3. Uncomment to set wallpaper with nitrogen
-nitrogen --restore &
-
-qbittorrent &
-
+# qbittorrent &
