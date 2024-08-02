b5e2bfb5c1c4bfe8cafe46b3742117e144681dd3
Fahim Fuad Prottoy <46813360+prottoyfuad@users.noreply.github.com>
2024-08-02 20:57:37 +0600

    Upd. Autostart script of Qtile

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -8,8 +8,5 @@ volumeicon &
 picom &
 nitrogen --restore &
 
-# festival --tts $HOME/.config/qtile/welcome_msg &
 # lxsession &
-# /usr/bin/emacs --daemon &
-# conky -c $HOME/.config/conky/qtile/doom-one-01.conkyrc
 # qbittorrent &
